56d841c21001d8930079fdad9a5cb867d3a3ac82
Manan Kandpal <manank.7409@gmail.com>
2025-02-23 12:47:32 +0530

    rollback

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import http from "http";
 
 import { Server } from 'socket.io';
 import { UserManager } from "./managers/UserManger";
-import admin from "./firebase";
 
 const server = http.createServer(http);
 
@@ -16,30 +15,8 @@ const io = new Server(server, {
 
 const userManager = new UserManager();
 
-var user: any;
-
-io.use((socket, next) => {
-  const userId = socket.handshake.query.userId as string;
-  const db = admin.firestore();
-  const userDocRef = db.collection('users').doc(userId);
-  userDocRef.get()
-    .then((doc) => {
-      if (doc.exists) {
-        user = doc.data();
-      } else {
-        console.log('No such document!');
-      }
-      next();
-    })
-    .catch((error) => {
-      console.error('Authentication error:', error);
-      next(new Error('Authentication error'));
-    });
-});
-
 io.on('connection', (socket: Socket) => {
-
-  console.log('a user connected', user?.name);
+  console.log('a user connected');
   userManager.addUser("randomName", socket);
   socket.on("disconnect", () => {
     console.log("user disconnected");
@@ -48,5 +25,5 @@ io.on('connection', (socket: Socket) => {
 });
 const PORT = Number(process.env.port) || 3000
 server.listen(PORT, "0.0.0.0", () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
